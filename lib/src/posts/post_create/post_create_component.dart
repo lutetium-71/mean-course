72ced7ef35c8012d3b164ef297cd1aa7f33d570a
lutetium <lutetium_71@outlook.com>
2018-12-04 22:15:51 -0800

    moved angular to angulardart

---
+++ lib/src/posts/post_create/post_create_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app-post-create',
+  styleUrls: ['post_create_component.css'],
+  templateUrl: 'post_create_component.html',
+)
+class PostCreateComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
--- src/app/posts/post-create/post-create.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
-})
-export class PostCreateComponent { }
