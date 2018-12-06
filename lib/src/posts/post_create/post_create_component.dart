2a905bd04360c36cdce596cb48ef515b20860177
lutetium <lutetium_71@outlook.com>
2018-12-05 22:02:48 -0800

    added simple material styling

--- lib/src/posts/post_create/post_create_component.dart
+++ lib/src/posts/post_create/post_create_component.dart
@@ -1,11 +1,22 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_icon/material_icon.dart';
+import 'package:angular_components/material_input/material_input.dart';
 
 @Component(
     selector: 'app-post-create',
-    styleUrls: ['post_create_component.css'],
+    styleUrls: [
+      'package:angular_components/css/mdc_web/card/mdc-card.scss.css',
+      'post_create_component.css'
+    ],
     templateUrl: 'post_create_component.html',
-    directives: [formDirectives])
+    directives: [
+      formDirectives,
+      MaterialButtonComponent,
+      MaterialIconComponent,
+      materialInputDirectives,
+    ])
 class PostCreateComponent {
   var enteredValue = '';
   var newPost = 'NO CONTENT';
