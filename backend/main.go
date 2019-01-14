6c0761247d0a9206e7c29b0a4cc085e5dad10619
Raion314 <raion314@outlook.com>
2019-01-13 16:22:20 -0800

    add create and read single document

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,50 +8,51 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"github.com/gorilla/mux"
-	"github.com/mongodb/mongo-go-driver/bson"
-	"github.com/mongodb/mongo-go-driver/bson/primitive"
-	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
 type (
 	// DB struct
 	DB struct {
-		client     *mongo.Client
-		colleciton *mongo.Collection
+		session    *mgo.Session
+		collection *mgo.Collection
 	}
 	// Post struct
 	Post struct {
-		ID      primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-		Name    string             `bson:"name" json:"name"`
-		Content string             `bson:"content" json:"content"`
+		ID      bson.ObjectId `bson:"_id,omitempty" json:"id"`
+		Title   string        `bson:"title" json:"title"`
+		Content string        `bson:"content" json:"content"`
 	}
 )
 
 func (db *DB) getAllPost(w http.ResponseWriter, r *http.Request) {
-	var posts []*Post
-	w.WriteHeader(http.StatusOK)
-	cur, err := db.colleciton.Find(context.TODO(), nil)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-	} else {
-		for cur.Next(context.TODO()) {
-			var post Post
-			err := cur.Decode(&post)
-			if err != nil {
-				log.Fatal(err)
-			}
-			posts = append(posts, &post)
-		}
-		if err := cur.Err(); err != nil {
-			w.Write([]byte(err.Error()))
-		}
-		cur.Close(context.TODO())
-		w.Header().Set("Content-Type", "application/json")
-		response, _ := json.Marshal(posts)
-		w.Write(response)
-	}
-	fmt.Printf("Found a single document: %+v\n", posts)
+	// var posts []*Post
+	// w.WriteHeader(http.StatusOK)
+	// cur, err := db.collection.Find(nil)
+	// if err != nil {
+	// 	w.Write([]byte(err.Error()))
+	// } else {
+	// 	for cur.Next(context.TODO()) {
+	// 		var post Post
+	// 		err := cur.Decode(&post)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 		posts = append(posts, &post)
+	// 	}
+	// 	if err := cur.Err(); err != nil {
+	// 		w.Write([]byte(err.Error()))
+	// 	}
+	// 	cur.Close(context.TODO())
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	response, _ := json.Marshal(posts)
+	// 	w.Write(response)
+	// }
+	// fmt.Printf("Found a single document: %+v\n", posts)
+	fmt.Fprintln(w, "not implemented yet!")
+
 }
 
 func (db *DB) createPost(w http.ResponseWriter, r *http.Request) {
@@ -61,8 +61,8 @@ func (db *DB) createPost(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(postBody, &post)
 
 	// create Hash ID for new post
-	post.ID = primitive.NewObjectID()
-	_, err := db.colleciton.InsertOne(context.TODO(), post)
+	post.ID = bson.NewObjectId()
+	err := db.collection.Insert(post)
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	} else {
@@ -70,7 +70,6 @@ func (db *DB) createPost(w http.ResponseWriter, r *http.Request) {
 		response, _ := json.Marshal(post)
 		w.Write(response)
 	}
-
 }
 func (db *DB) updatePost(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet!")
@@ -81,12 +80,10 @@ func (db *DB) deletePost(w http.ResponseWriter, r *http.Request) {
 
 func (db *DB) findPost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id, _ := primitive.ObjectIDFromHex(vars["id"])
 	var post Post
-	filter := bson.M{"_id": id}
 
 	w.WriteHeader(http.StatusOK)
-	err := db.colleciton.FindOne(context.TODO(), filter).Decode(&post)
+	err := db.collection.Find(bson.M{"_id": bson.ObjectIdHex(vars["id"])}).One(&post)
 	if err != nil {
 		w.Write([]byte(err.Error()))
 	} else {
@@ -94,25 +91,18 @@ func (db *DB) findPost(w http.ResponseWriter, r *http.Request) {
 		response, _ := json.Marshal(post)
 		w.Write(response)
 	}
-	fmt.Printf("Found a single document: %+v\n", post)
 }
 
 func main() {
-	ctx, ctxErr := context.WithTimeout(context.Background(), 10*time.Second)
-	if ctxErr != nil {
-		log.Fatal(ctxErr)
-	}
-	client, err := mongo.Connect(ctx, "mongodb://localhost:27017")
+	session, err := mgo.Dial("localhost:27017")
+	collection := session.DB("post").C("posts")
+	db := &DB{session: session, collection: collection}
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	collection := client.Database("post").Collection("posts")
-	db := &DB{client: client, colleciton: collection}
-
 	fmt.Println("[Connected] to MongoDB")
 
-	defer client.Disconnect(context.TODO())
+	defer session.Close()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/posts", db.getAllPost).Methods("GET")
@@ -122,7 +112,11 @@ func main() {
 	r.HandleFunc("/api/posts/{id}", db.findPost).Methods("GET")
 
 	fmt.Println("Server running on localhost:3000")
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
+	log.Fatal(srv.ListenAndServe())
 }
