8f4a6e2ab2b5d7e6adf7d66225f17fb34842641a
Raion314 <raion314@outlook.com>
2019-01-13 21:55:49 -0800

    add browser_client

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,13 @@
 import 'package:angular/angular.dart';
 import 'package:posts/app_component.template.dart' as ng;
+import 'package:http/browser_client.dart';
 
+import 'main.template.dart' as self;
+
+@GenerateInjector([
+  ClassProvider(BrowserClient),
+])
+final InjectorFactory injector = self.injector$Injector;
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.AppComponentNgFactory, createInjector: injector);
 }
