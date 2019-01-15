64b937f9cc934bde68a1838a9c47f77e7eb6d5b9
Raion314 <raion314@outlook.com>
2019-01-14 19:17:32 -0800

    add Post factory

--- lib/src/post_model.dart
+++ lib/src/post_model.dart
@@ -1,6 +1,12 @@
 class Post {
+  String id;
   String title;
   String content;
 
-  Post(this.title, this.content);
+  Post(this.id, this.title, this.content);
+
+  factory Post.fromJson(Map<String, dynamic> post) =>
+      Post(post['id'], post['title'], post['content']);
+
+  Map toJson() => {'id': id, 'title': title, 'content': content};
 }
