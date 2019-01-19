9b085e24d362eb65085e083fc55d13f2b89fe683
Raion314 <raion314@outlook.com>
2019-01-19 13:17:04 -0800

    removed post edit component

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -4,8 +4,6 @@ import 'posts/post_list/post_list_component.template.dart'
     as post_list_template;
 import 'posts/post_create/post_create_component.template.dart'
     as post_create_template;
-import 'posts/post_edit/post_edit_component.template.dart'
-    as post_edit_template;
 import 'not_found_component.template.dart' as not_found_template;
 
 import 'route_paths.dart';
@@ -21,8 +19,8 @@ class Routes {
     component: post_create_template.PostCreateComponentNgFactory,
   );
   static final edit = RouteDefinition(
-    routePath: RoutePaths.edit,
-    component: post_edit_template.PostEditComponentNgFactory,
+    routePath: RoutePaths.create,
+    component: post_create_template.PostCreateComponentNgFactory,
   );
   static final notfound = RouteDefinition(
     path: '.+',
