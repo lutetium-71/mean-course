168604a5aef7c8f5b1d18bc5cd81b68f0ac1dc69
Raion314 <raion314@outlook.com>
2019-01-18 22:24:48 -0800

    update route paths

--- lib/src/route_paths.dart
+++ lib/src/route_paths.dart
@@ -1,6 +1,14 @@
 import 'package:angular_router/angular_router.dart';
 
+const postId = 'id';
+
 class RoutePaths {
   static final posts = RoutePath(path: '');
   static final create = RoutePath(path: 'create');
+  static final edit = RoutePath(path: 'edit/:$postId');
+}
+
+String getId(Map<String, String> parameters) {
+  final id = parameters[postId];
+  return id == null ? null : id;
 }
