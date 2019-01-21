1354a7d281de77c85157828f30f1a114db5c3aea
Raion314 <raion314@outlook.com>
2019-01-20 22:03:16 -0800

    add login and signup components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -5,6 +5,8 @@ import 'package:angular_router/angular_router.dart';
 import 'src/posts/post_create/post_create_component.dart';
 import 'src/posts/post_list/post_list_component.dart';
 import 'src/header/header_component.dart';
+import 'src/auth/signup/signup_component.dart';
+import 'src/auth/login/login_component.dart';
 import 'src/post_list_service.dart';
 import 'src/routes.dart';
 
@@ -13,9 +15,11 @@ import 'src/routes.dart';
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
   directives: [
+    HeaderComponent,
     PostCreateComponent,
     PostListComponent,
-    HeaderComponent,
+    LoginComponent,
+    SignUpComponent,
     routerDirectives,
   ],
   providers: [materialProviders, ClassProvider(PostListService)],
